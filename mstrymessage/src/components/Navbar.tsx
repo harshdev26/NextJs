b222885b7b8ae8c54e8448692fa22f7d7b4f3259
Harshii909 <tiwariharsh2604.gmail.com>
2025-10-07 22:56:21 +0530

    ,........

---
+++ mstrymessage/src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+"use client"
+import  React from 'react'
+import Link from 'next/link'
+import { useSession, signOut } from "next-auth/react"
+import {User} from 'next-auth'
+import { Button } from '@react-email/button'
+
+const Navbar = () => {
+
+  const {data: session} = useSession()
+  const user: User = session?.user as User
+  
+  return (
+    <nav>
+        <div>
+            <a href="#">Mystry Message</a>
+            {
+                session? (
+                    <>
+                    <span>Welcome, {user?.username || user?.email}</span>
+                    <Button onClick={() => signOut()}>Logout</Button>
+                    </>
+                ) : (
+                    <Link href={'/sign-in'}>
+                        <Button>Login</Button>
+                    </Link>
+                )
+            }
+        </div>
+    </nav>
+  )
+
+}
+
+export default Navbar;
